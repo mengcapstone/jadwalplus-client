8a0c519e76ba5b6d9b8987cf0085d42bc756e93e
adelintang <adelintang157@gmail.com>
2024-04-05 22:29:31 +0700

    perf: navigation bottom

--- src/components/NavigationBottom.jsx
+++ src/components/NavigationBottom.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {
   FaHome, FaPlusCircle, FaArchive, FaSignOutAlt,
 } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../pages/styles/auth.css';
 
 const NavigationBottom = ({ logout }) => {
   const navigate = useNavigate();
+  const path = useLocation().pathname;
 
   const logoutHandler = () => {
     logout();
@@ -19,20 +20,20 @@ const NavigationBottom = ({ logout }) => {
       <div className="navigation-icons container-icon">
         <div className="navigation-icon" id="add-icon">
           <Link to="/" className="reset-link wrap-icon">
-            <FaHome className="icon-navigation-action" />
-            <span className="icon-label">Home</span>
+            <FaHome className={`icon-navigation-action hover:text-blue-500 ${path === '/' ? 'text-blue-800' : 'text-black'}`} />
+            <span className={`icon-label hover:text-blue-500 ${path === '/' ? 'text-blue-800' : 'text-black'}`}>Home</span>
           </Link>
         </div>
         <div className="navigation-icon" id="add-book-icon">
           <Link to="/schedule/add" className="wrap-icon reset-link">
-            <FaPlusCircle className="icon-navigation-action" />
-            <span className="icon-label">Add</span>
+            <FaPlusCircle className={`icon-navigation-action hover:text-blue-500 ${path === '/schedule/add' ? 'text-blue-800' : 'text-black'}`} />
+            <span className={`icon-label hover:text-blue-500 ${path === '/schedule/add' ? 'text-blue-800' : 'text-black'}`}>Add</span>
           </Link>
         </div>
         <div className="navigation-icon" id="add-book-icon">
           <Link to="/schedules/arsip" className="wrap-icon reset-link">
-            <FaArchive className="icon-navigation-action" />
-            <span className="icon-label">Archive</span>
+            <FaArchive className={`icon-navigation-action hover:text-blue-500 ${path === '/schedules/arsip' ? 'text-blue-800' : 'text-black'}`} />
+            <span className={`icon-label hover:text-blue-500 ${path === '/schedules/arsip' ? 'text-blue-800' : 'text-black'}`}>Archive</span>
           </Link>
         </div>
         <div className="navigation-icon" id="logOut-icon">
